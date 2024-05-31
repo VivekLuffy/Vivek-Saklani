c3060eae7e70f2c3fb1ffc8f44c000af1dc5c45e
VivekLuffy <viveksaklani874@gmail.com>
2024-05-31 21:24:06 +0530

    Add files via upload

---
+++ inter22.js
@@ -0,0 +1,10 @@
+let currentIndex = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = slides.children.length;
+
+function showNextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+setInterval(showNextSlide, 2000);
